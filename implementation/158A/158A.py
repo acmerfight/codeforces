26f4e1ec02bbf112d115f4fbc4176c0cda2c2367
Andy Yao <yaogangqiang@admaster.com.cn>
2013-04-16 11:37:17 +0800

    删除不必要文件

--- implementation/158A/158A.py
+++
@@ -1,9 +0,0 @@
-n, k = map(int, raw_input().split())
-data_list = map(int, raw_input().split())
-count = 0
-for data in data_list:
-    if data >= data_list[k-1] and data > 0:
-        count += 1
-    elif data < data_list[k-1]:
-        break
-print count
